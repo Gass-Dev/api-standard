dbc12b24c0df9572fe71f8b6c81dd90e8fe3d1c4
Gass <madebygass@gmail.com>
2024-03-20 23:24:08 +0100

    update cinfig and install websocket

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const { connect } = require("./db/connect");
 const userRouter = require("./routes/user");
 
@@ -11,6 +13,31 @@ app.use(express.json());
 // Définition des routes
 app.use("/api/v1", userRouter);
 
+// Création du serveur HTTP
+const server = http.createServer(app);
+
+// Initialisation de Socket.io
+const io = socketIo(server);
+
+// Middleware pour gérer les connexions WebSocket
+io.on("connection", (socket) => {
+  console.log("Nouvelle connexion WebSocket établie");
+
+  // Gérer les événements WebSocket ici...
+
+  // Exemple d'événement de chat
+  socket.on("chat message", (msg) => {
+    console.log("Message reçu:", msg);
+    // Émettre le message à tous les clients connectés
+    io.emit("chat message", msg);
+  });
+
+  // Gérer la déconnexion
+  socket.on("disconnect", () => {
+    console.log("Client déconnecté");
+  });
+});
+
 // Route de test
 app.get("/", (req, res) => {
   console.log("Hello");
@@ -20,6 +47,7 @@ app.get("/", (req, res) => {
 // Connexion à la base de données
 connect();
 
-app.listen(PORT, () => {
+// Démarrage du serveur
+server.listen(PORT, () => {
   console.log("Lancé sur le port", PORT);
 });
