1d1832f834a12ee22623da8803d5e7847d04323b
Gass <madebygass@gmail.com>
2024-03-22 17:21:40 +0100

    add jwt and bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,20 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 // Créer un nouvel utilisateur
 const createUser = async (req, res) => {
-  console.log(req.body)
   try {
     const { username, email, password } = req.body;
-    console.log(username, email, password)
-    const user = new User({ username, email, password });
-    await user.save().then(() => {
-      res.status(201).json(user);
-    });
+
+    // Hacher le mot de passe
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Créer un nouvel utilisateur avec le mot de passe haché
+    const user = new User({ username, email, password: hashedPassword });
+
+    await user.save();
+    res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -43,7 +48,16 @@ const getUserById = async (req, res) => {
 const updateUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    await User.findByIdAndUpdate(req.params.id, { username, email, password });
+    // Hacher le nouveau mot de passe si fourni
+    const hashedPassword = password
+      ? await bcrypt.hash(password, 10)
+      : undefined;
+
+    await User.findByIdAndUpdate(req.params.id, {
+      username,
+      email,
+      password: hashedPassword,
+    });
     res.json({ message: "Utilisateur mis à jour" });
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -60,4 +74,35 @@ const deleteUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser, getUsers, getUserById, updateUser, deleteUser };
+// Connexion de l'utilisateur
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Vérifie si l'utilisateur existe dans la base de données
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+    // Vérifie si le mot de passe est correct
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: "Mot de passe incorrect" });
+    }
+    // Génère un token JWT pour l'utilisateur authentifié
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  loginUser,
+};
---
+++ services/authService.js
@@ -0,0 +1,14 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+// Connexion de l'utilisateur
+const loginUser = async (email, password) => {
+  try {
+    // Logique de connexion d'utilisateur
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { loginUser };
